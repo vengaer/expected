f1a7e400d13c3a169e4fe8352f18f0f9c224de05
VilhelmEngstrom <vilhelm.engstrom@tuta.io>
2019-08-17 11:20:56 +0200

    Add test for checking that value throws correctly

--- tests/expected_accessors.cc
+++ tests/expected_accessors.cc
@@ -1,12 +1,13 @@
 #ifndef EXPECTED_MANUAL_TEST
 #include "catch.hpp"
 #include "expected.h"
+#include "traits.h"
 #include <string>
 #include <type_traits>
 
 using namespace vien;
 
-TEMPLATE_TEST_CASE("value() returns T", "[expected][value]", int, std::string) {
+TEMPLATE_TEST_CASE("value accessor returns T", "[expected][value][accessor]", int, std::string) {
     REQUIRE(std::is_same_v<TestType&&, 
                            decltype(std::declval<expected<TestType, double>>().value())>);
     REQUIRE(std::is_same_v<TestType const&&, 
@@ -17,12 +18,22 @@ TEMPLATE_TEST_CASE("value() returns T", "[expected][value]", int, std::string) {
                            decltype(std::declval<expected<TestType, double> const&>().value())>);
 }
 
-TEST_CASE("value() returns void iff T is void", "[expected][void]") {
+TEST_CASE("value accessor returns void iff T is void", "[expected][value][accessor]") {
     REQUIRE(std::is_same_v<void, decltype(std::declval<expected<void, double>>().value())>);
     REQUIRE(!std::is_same_v<void, decltype(std::declval<expected<int, double>>().value())>);
 }
 
-TEMPLATE_TEST_CASE("error() returns E", "[expected][error]", int, std::string) {
+TEST_CASE("value accessor throws if !bool(*this)", "[expected][value][accessor]") {
+    unexpected<int> u(1);
+    expected<double, int> e(u);
+    REQUIRE(
+        THROWS(
+            e.value(), bad_expected_access<int>
+        )
+    );
+}
+
+TEMPLATE_TEST_CASE("error accessor returns E", "[expected][error][accessor]", int, std::string) {
     REQUIRE(std::is_same_v<TestType&&, 
                            decltype(std::declval<expected<double, TestType>>().error())>);
     REQUIRE(std::is_same_v<TestType const&&, 
